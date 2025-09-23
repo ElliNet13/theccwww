c4a6eb51297ad3f73adf74eb7a0fe862d7bb75f4
ElliNet13 (PC) <elliotmoose4@gmail.com>
2025-09-23 16:31:44 -0600

    Fix canceling error

--- server.lua
+++ server.lua
@@ -9,6 +9,8 @@ local dir = fs.getDir(shell.getRunningProgram())
 local files = fs.combine(dir, "files")
 
 if not fs.exists(dir .. "/config.yaml") then
+    print("Creating config.yaml with default config")
+    print("Make sure to change your hostname in the file!")
     fs.copy(dir .. "/server.default.yaml", dir .. "/config.yaml")
 end
 
@@ -22,9 +24,20 @@ print("Files: " .. files)
 
 -- Check if files exist
 if not fs.exists(files) then
+    print("Creating files directory with default files")
     fs.makeDir(files)
-    fs.copy(dir .. "/index.default.lua", fs.combine(files, "index.lua"))
-    fs.copy(dir .. "/404.default.lua", fs.combine(files, "404.lua"))
+end
+
+-- Create 404 page
+if not fs.exists(files .. "/404.lua") then
+    print("Creating 404 page")
+    fs.copy(files .. "/404.default.lua", files .. "/404.lua")
+end
+
+-- Create index page
+if not fs.exists(files .. "/index.lua") then
+    print("Creating index page")
+    fs.copy(files .. "/index.default.lua", files .. "/index.lua")
 end
 
 -- Start hosting
@@ -47,14 +60,14 @@ local function serverLoop()
         else
             -- It's a 404
             print("Sending 404 to " .. id .. " because " .. message .. " does not exist")
-            message = "404"
+            message = "404.lua"
         end
         local path = fs.combine(files, message)
-        print("Sending final path " .. path .. " to " .. id)
         if not string.find(path, files .. "/") then
             print("[CANCELED] Computer " .. id .. " requested file " .. message .. " outside of " .. files .. " directory")
-            path = fs.combine(files, "404")
+            path = fs.combine(files, "404.lua")
         end
+        print("Sending final path " .. path .. " to " .. id)
         local file = fs.open(path, "r")
         local content = file.readAll()
         file.close()
