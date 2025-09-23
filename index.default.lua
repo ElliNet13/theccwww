07a4eafc304b0c9b4df7fc28f5326fccb989a4d4
ElliNet13 (PC) <elliotmoose4@gmail.com>
2025-09-22 19:38:54 -0600

    Change defaults to use more funcs

--- index.default.lua
+++ index.default.lua
@@ -1,3 +1,6 @@
 print("Welcome to the default site!")
 print("This is the home page of the default site.")
 print("If you are the owner of this site, you can change the home page by editing files/index.lua")
+if theccwww.promptYesNo("Do you want to download this page?") then
+    theccwww.save(theccwww.link.domain, theccwww.link.page)
+end
